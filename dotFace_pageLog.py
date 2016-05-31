8ecdded060e84041a2efd11bc6f8b69e486eaa69
park junho <huinalam@gmail.com>
2016-05-31 11:51:35 +0900

    에러처리시 log 남김

--- dotFace_pageLog.py
+++ dotFace_pageLog.py
@@ -2,6 +2,17 @@
 import privateKey as pkey
 import json
 import time
+import logging
+
+logger = logging.getLogger('pagelog')
+logger.debug('pagelog')
+fh = logging.FileHandler('pagelog.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+
 
 # init
 fb_api_url = "https://graph.facebook.com/v2.6/"
@@ -48,66 +59,79 @@ access_token = pkey.start_token
 dotface_id = "214142335609595"
 
 # main
-while True:
-    access_token = get_refresh_token(access_token)
-    # Todo : 100개 이상 늘어날 시에 받아오기 위한 작업 필요
-    posts_json = get_fb(dotface_id, "posts", {"limit": 100}).json()["data"]
-
-    data = []
-    unique_key = "total_video_impressions_viral_unique"
-    story_key = "total_video_stories_by_action_type"
-    for post_json in posts_json:
-        post_id = post_json["id"][16:]
-        rq = get_fb(post_id, "video_insights", {"metric": unique_key + "," + story_key})
-        # 리턴 값이 없는 경우
-        if rq.ok is not True:
-            continue
-        rq_json = rq.json()
-        # 어차피 2개 밖에 없으니
+try:
+    while True:
+        access_token = get_refresh_token(access_token)
+        # Todo : 100개 이상 늘어날 시에 받아오기 위한 작업 필요
+        # 예외처리 작업 필요
         try:
-            if rq_json["data"][0]["name"] == unique_key:
-                viral_json = rq_json["data"][0]
-                stories_json = rq_json["data"][1]
-            else:
-                viral_json = rq_json["data"][1]
-                stories_json = rq_json["data"][0]
-                pass
-            post_unique = viral_json["values"][0]["value"]
-        except:
-            post_unique = 0
+            posts_json = get_fb(dotface_id, "posts", {"limit": 100}).json()["data"]
+        except Exception as e:            
+            logger.critical(e)            
+            time.sleep(60 * 1)
+            continue
 
-        # append story
-        # 값이 없을 경우, key로 존재하지 않으므로 이를 예외처리할 함수
-        def get_story_value(key, rq_json):
-            if list(rq_json.keys()).count(key) > 0:
-                return rq_json[key]
-            else:
-                return 0
+
+        data = []
+        unique_key = "total_video_impressions_viral_unique"
+        story_key = "total_video_stories_by_action_type"
+        for post_json in posts_json:
+            post_id = post_json["id"][16:]
+            rq = get_fb(post_id, "video_insights", {"metric": unique_key + "," + story_key})
+            # 리턴 값이 없는 경우
+            if rq.ok is not True:
+                continue
+            rq_json = rq.json()
+            # 어차피 2개 밖에 없으니
+            try:
+                if rq_json["data"][0]["name"] == unique_key:
+                    viral_json = rq_json["data"][0]
+                    stories_json = rq_json["data"][1]
+                else:
+                    viral_json = rq_json["data"][1]
+                    stories_json = rq_json["data"][0]
+                    pass
+                post_unique = viral_json["values"][0]["value"]
+            except Exception as e:
+                logger.warn(e)
+                post_unique = 0
+
+            # append story
+            # 값이 없을 경우, key로 존재하지 않으므로 이를 예외처리할 함수
+            def get_story_value(key, rq_json):
+                if list(rq_json.keys()).count(key) > 0:
+                    return rq_json[key]
+                else:
+                    return 0
+                pass
+
+            try:
+                story_value = stories_json["values"][0]["value"]
+                post_like = story_value.get("like")
+                post_share = story_value.get("share")
+                post_comment = story_value.get("comment")
+            except Exception as e:
+                logger.warn(e)
+                post_like = 0
+                post_share = 0
+                post_comment = 0        
+        
+            post_msg = post_json.get("message")
+            post_created_time = post_json["created_time"]
+
+            data.append((
+                post_id, post_like, post_share, post_comment, post_unique, post_msg, post_created_time
+                ))
             pass
 
-        try:
-            story_value = stories_json["values"][0]["value"]
-            post_like = story_value.get("like")
-            post_share = story_value.get("share")
-            post_comment = story_value.get("comment")
-        except:
-            post_like = 0
-            post_share = 0
-            post_comment = 0        
-        
-        post_msg = post_json.get("message")
-        post_created_time = post_json["created_time"]
+        json_dump_str = json.dumps(data, ensure_ascii=False).encode('utf8')
+        #json.dump(data, open('data/data.json', 'w+'))
 
-        data.append((
-            post_id, post_like, post_share, post_comment, post_unique, post_msg, post_created_time
-            ))
+        post_table_log(json_dump_str.decode(encoding="utf-8"))
+        # test    
+        #break
+        time.sleep(60 * 10)
         pass
+except Exception as e:
+    logger.critical(e)
 
-    json_dump_str = json.dumps(data, ensure_ascii=False).encode('utf8')
-    #json.dump(data, open('data/data.json', 'w+'))
-
-    post_table_log(json_dump_str.decode(encoding="utf-8"))
-    # test    
-    break
-    time.sleep(60 * 10)
-    pass
